0dd17b55b5b7a01cf5ceeaf78216550e02bf67c6
Nicolas Gomez <nicolasgomez8873030@gmail.com>
2023-06-05 15:42:55 -0500

    [+] se añadio toques al juego

--- triqui.js
+++ triqui.js
@@ -9,24 +9,24 @@ let tablero = [
 function winner(){
     //horizontales
     for(let i = 0; i < 3; i++){
-        if(tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero [i][2]){
+        if(tablero[i][0] === tablero[i][1] && tablero[i][1] === tablero [i][2]){
             return tablero[i][0];
         }
     }
     //vertical
     for(let i = 0; i < 3; i++){
-        if(tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero [2][i]){
+        if(tablero[0][i] === tablero[1][i] && tablero[1][i] === tablero [2][i]){
             return tablero[i][0];
         }
     }
     // Diagonal de izquierda a derecha
-	if( tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2] ){
+	if( tablero[0][0] === tablero[1][1] && tablero[1][1] === tablero[2][2] ){
 		return tablero[0][0];
 
 	}
 
 	// Diagonal de Derecha a Izquierda
-	if(tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0] ){
+	if(tablero[0][2] === tablero[1][1] && tablero[1][1] === tablero[2][0] ){
 		return tablero[0][2];
 	}
 }
